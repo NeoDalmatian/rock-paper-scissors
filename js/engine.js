57e33d41a26eb50119f2e4ea2e449bfb30d77d8f
NeoDalmatian <aviskovic100@gmail.com>
2022-07-14 18:50:38 +0200

    make functional game function with for loop, super proud with this one :P

--- js/engine.js
+++ js/engine.js
@@ -1,8 +1,4 @@
 
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
 function computerPlay () {
   randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber == 1) {
@@ -14,16 +10,43 @@ function computerPlay () {
   }
 }
 
- function playRound (playerSelection, computerSelection) {
-   if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Scissors" && computerSelection == "Scissors") {
-    return "It\'s a Tie!";
-   } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-   }
- }
+ function playRound (playerSelection, computerSelection = computerPlay()) {
+  if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase() || playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase() || playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase()) {
+   return "It\'s a Tie!";
+  } else if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase() || playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase() || playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase()) {
+   return "Win";
+  } else if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase() || playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase() || playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase()) {
+   return "Loss";
+  } else {
+   return "Wrong Input! Write either Rock, Paper or Scissors"
+  }
+}
 
-const playerSelection = "Scissors";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game () {
+  let pPoints = 0;
+  let cPoints = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Write either Rock, Paper or Scissors");
+    let result = playRound(playerSelection);
+
+    if (result == "Win") {
+      pPoints++
+      console.log(`You Won this round! Player:${pPoints} Computer: ${cPoints}`)
+    } else if (result == "Loss") {
+      cPoints++
+      console.log(`You Lost this round! Player:${pPoints} Computer: ${cPoints}`)
+    } else {
+      console.log("It\'s a Tie!")
+    }
+  }
+
+  if (pPoints > cPoints) {
+    console.log("You Win this game!")
+  } else if (pPoints < cPoints) {
+    console.log("You Lost this game!")
+  } else {
+    console.log("It\s a Tie!")
+  }
+}
+
