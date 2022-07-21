254cbcca43b964d0cf318b6b362adb8699111127
NeoDalmatian <aviskovic100@gmail.com>
2022-07-21 14:03:22 +0200

    add eventListener for buttons

--- js/engine.js
+++ js/engine.js
@@ -17,49 +17,36 @@ function playRound (playerSelection, computerSelection = computerPlay()) {
   if (playerSelection == "ROCK" && computerSelection == "SCISSORS" ||
       playerSelection == "PAPER" && computerSelection == "ROCK" ||
       playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-   return "Win";
+   //return "Win";
+   console.log("Win")
   } else if (playerSelection == "ROCK" && computerSelection == "PAPER" ||
       playerSelection == "PAPER" && computerSelection == "SCISSORS" ||
       playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-   return "Loss";
+   //return "Loss";
+   console.log("Loss")
   } else if (playerSelection == "ROCK" && computerSelection == "ROCK" ||
       playerSelection == "PAPER" && computerSelection == "PAPER" ||
       playerSelection == "SCISSORS" && computerSelection == "SCISSORS") {
-    return "Tie";
+    //return "Tie";
+    console.log("Tie")
   } else {
     return "Wrong input";
   }
 }
 
-//Function that loops function "playRound()" 5 times with input of "playerSelection" 
-//being prompt, score is saved and winner is declared depending on rules of the game.
-function game () {
-  let playerPoints = 0;
-  let computerPoints = 0;
+const buttons = document.querySelectorAll("button");
+const player = document.querySelector("#player");
+const computer = document.querySelectorAll("#computer");
 
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Write either Rock, Paper or Scissors").toUpperCase();
-    let result = playRound(playerSelection);
-
-    if (result == "Win") {
-      playerPoints++
-      console.log(`You Won this round! Player:${playerPoints} Computer: ${computerPoints}`)
-    } else if (result == "Loss") {
-      computerPoints++
-      console.log(`You Lost this round! Player:${playerPoints} Computer: ${computerPoints}`)
-    } else if (result == "Tie"){
-      console.log("It\'s a Tie!")
-    } else {
-      --i
-      console.log("Wrong Input! Write either Rock, Paper or Scissors")
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //console.log(e.path[0].id);
+    if (e.path[0].id === "rock") {
+      playRound("ROCK");
+    } else if (e.path[0].id === "paper") {
+      playRound("PAPER");
+    } else if (e.path[0].id === "scissors") {
+      playRound("SCISSORS");
     }
-  }
-
-  if (playerPoints > computerPoints) {
-    console.log("You Win this game!")
-  } else if (playerPoints < computerPoints) {
-    console.log("You Lost this game!")
-  } else {
-    console.log("It\s a Tie!")
-  }
-}
+  })
+})
